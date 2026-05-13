2f2e8a91a6fa635d2661c6135b4567c99dfc9539
JackieTrinh <jtrinh3@nait.ca>
2026-05-12 18:54:42 -0600

    1

---
+++ lesson-03-starter/src/main.js
@@ -0,0 +1,19 @@
+const { loadConfigFromFile } = require("vite");
+
+console.log('Lesson 03 starter loaded');
+//console.log(typeof(1));
+
+console.log("123x");
+//const name = "123";
+//const newVar = parseInt(name,10);
+
+//mkvknjvnj ;
+// Instructor TODO: 
+// 1. Declare variables using var, let, const
+// 2. Log their types with console.log(typeof …)
+// 3. Try built-in functions: alert(), prompt(), parseInt(), toString()
+// 4. Manipulate values and observe results in the console
+
+// Student TODO: 
+// Prompt the user for their name and age
+// Log a greeting message using the provided name and age
